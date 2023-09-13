8a6e8f7caca62e496c14422bf3c02a4eed91ee28
Sujith-Rajan <sujithrajan223@gmail.com>
2023-09-13 20:45:26 +0530

    libaray structure created

---
+++ libaray.js
@@ -0,0 +1,23 @@
+let book =document.getElementById('book')
+let author =document.getElementById('author')
+let pages=document.getElementById('pages')
+
+const myLibrary = [
+    {
+        title: "",
+        author: "",
+        pages: "",
+        read: true,
+    },
+];
+function Book() {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+function addBookToLibrary() {
+    myLibrary.title.value =
+    myLibrary.author.value =
+    myLibrary.pages.value=
+}
