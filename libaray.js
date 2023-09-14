793f4b299fb1d271ce1f18b9ad91a319570a5010
Sujith-Rajan <sujithrajan223@gmail.com>
2023-09-14 12:09:45 +0530

    simple javascript include library

--- libaray.js
+++ libaray.js
@@ -1,23 +1,59 @@
-let book =document.getElementById('book')
-let author =document.getElementById('author')
-let pages=document.getElementById('pages')
+const bookInput =document.getElementById('book')
+const authorInput =document.getElementById('author')
+const pagesInput=document.getElementById('pages')
+const submit=document.getElementById('submit')
+const card= document.getElementById('card')
 
-const myLibrary = [
-    {
-        title: "",
-        author: "",
-        pages: "",
-        read: true,
-    },
-];
-function Book() {
+
+
+
+const myLibrary = [];
+
+function Book(title,author,pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
 }
 
 function addBookToLibrary() {
-    myLibrary.title.value =
-    myLibrary.author.value =
-    myLibrary.pages.value=
+ const title =bookInput.value
+   const author =authorInput.value
+    const pages=pagesInput.value
+   
+    if( title && author && pages){
+   
+     const newBook = new Book(title,author,pages)
+     myLibrary.push(newBook)
+     renderLibarary()
+}else{
+    alert("Please fill the fields")
 }
+}
+
+function renderLibarary(){
+     card.innerHTML='';
+    
+    myLibrary.forEach((book,index) =>{
+        const divElement = document.createElement('div')
+    divElement.className = 'added-details'
+  
+    const titleElement = document.createElement('h2')
+    const authorElement = document.createElement('h4')
+    const pagesElement = document.createElement('h6')
+
+    titleElement.textContent =`${book.title}`
+    authorElement.textContent =`${book.author}`
+    pagesElement.textContent =`${book.pages}`
+    divElement.appendChild(titleElement)
+    divElement.appendChild(authorElement)
+    divElement.appendChild(pagesElement)
+    
+    card.appendChild(divElement)
+})
+}
+
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    addBookToLibrary();
+});
